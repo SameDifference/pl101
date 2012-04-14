cfcb1ade38ced98ea60b54c121db19bcae31b7c0
Same Difference <keysoulv@live.com>
2012-04-14 13:40:56 -0700

    adds supports for rests + modifies sequence to show off rests

--- mus/compiler.js
+++ mus/compiler.js
@@ -3,7 +3,7 @@ var t = 0;
 function endTime(time, expr) {
     var curTime = time;
     var json = JSON.parse(JSON.stringify(expr));
-    if (json.tag == 'note') {
+    if (json.tag == 'note' || json.tag == 'rest') {
         curTime += json.dur;
     } else if (json.tag == 'seq') {
         curTime += endTime(0, json.right) + endTime(0, json.left);
@@ -42,6 +42,12 @@ function translate(musexpr) {
             t += rt;
         }
         return left + ", " + right;
+    } else if (musexpr.tag == 'rest') {
+        note.tag = 'note';
+        note.pitch = 0;
+        note.start = t;
+        note.dur = musexpr.dur;
+        return JSON.stringify(note);
     }
 }
 
@@ -53,11 +59,11 @@ var melody_mus =
     { tag: 'seq',
       left: 
        { tag: 'seq',
-         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         left: { tag: 'rest', dur: 250 },
          right: { tag: 'note', pitch: 'b4', dur: 250 } },
       right:
        { tag: 'seq',
-         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         left: { tag: 'rest', pitch: 'c4', dur: 500 },
          right: { tag: 'note', pitch: 'd4', dur: 500 } } };
 
 /* rhino: let console.log be used instead of print */
