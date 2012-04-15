35caaf3a1ffb0a6bd7b1427f14e441b65eab6324
Same Difference <keysoulv@live.com>
2012-04-15 11:20:17 -0700

    Large structural changing.
    Removed endTime. pitch2midi is more accurate. translate now accepts notes, which is a list of current notes that will be added to the end. No longer relies on JSON (except for the display). Updated test case. Updated comment.

--- mus/compiler.js
+++ mus/compiler.js
@@ -1,75 +1,89 @@
 var t = 0;
 
-function endTime(time, expr) {
-    var curTime = time;
-    var json = JSON.parse(JSON.stringify(expr));
-    if (json.tag == 'note' || json.tag == 'rest') {
-        curTime += json.dur;
-    } else if (json.tag == 'seq') {
-        curTime += endTime(0, json.right) + endTime(0, json.left);
-    } else if (json.tag == 'par') {
-        curTime += (endTime(0, json.right) > endTime(0, json.left) ? endTime(0, json.right) : endTime(0, json.left));
-    }
-    return curTime;
-}
-
 function pitch2midi(pitch) {
-	return 12 + 12 * (pitch.charCodeAt(1) - 48) + (pitch.charCodeAt(0) - 99);
+	return 12 + 12 * (pitch.charCodeAt(1) - 48) + (pitch.charCodeAt(0) - 97);
 }
 
-function translate(musexpr) {
+function translate(musexpr, notes) {
     var note = {};
     var left, right, lt, rt;
-    if (musexpr.tag == 'note') {
-        note.tag = 'note';
-        note.pitch = pitch2midi(musexpr.pitch);
-        note.start = t;
-        note.dur = musexpr.dur;
-        return JSON.stringify(note);
-    } else if (musexpr.tag == 'seq') {
-        left = translate(musexpr.left);
-        if (musexpr.left.dur === undefined) musexpr.left.dur = 0;
-        t += musexpr.left.dur;
-        right = translate(musexpr.right);
-        t += musexpr.right.dur;
-        return left + ", " + right;
-    } else if (musexpr.tag == 'par') {
-        left = translate(musexpr.left);
-        if (musexpr.left.dur === undefined) musexpr.left.dur = 0;
-        lt = musexpr.left.dur;
-        right = translate(musexpr.right);
-        rt = musexpr.right.dur;
-        if (lt > rt) {
-            t += lt;
-        } else {
-            t += rt;
-        }
-        return left + ", " + right;
-    } else if (musexpr.tag == 'rest') {
-        note.tag = 'note';
-        note.pitch = 0;
-        note.start = t;
-        note.dur = musexpr.dur;
-        return JSON.stringify(note);
-    }
+    switch (musexpr.tag) {
+		case 'rest':
+		case 'note':
+			note.tag = 'note';
+			if (musexpr.pitch === undefined) {
+				note.pitch = 0;
+			} else {
+				note.pitch = pitch2midi(musexpr.pitch);
+			}
+			note.start = t;
+			note.dur = musexpr.dur;	
+			notes.push(note);
+			return;
+		case 'seq':
+			left = translate(musexpr.left, notes);
+			if (musexpr.left.dur === undefined) musexpr.left.dur = 0;
+			t += musexpr.left.dur;
+			right = translate(musexpr.right, notes);
+			if (musexpr.right.dur === undefined) musexpr.right.dur = 0;
+			t += musexpr.right.dur;
+			break;
+		case 'par':
+			left = translate(musexpr.left, notes);
+			if (musexpr.left.dur === undefined) musexpr.left.dur = 0;
+			lt = musexpr.left.dur;
+			right = translate(musexpr.right, notes);
+			if (musexpr.right.dur === undefined) musexpr.right.dur = 0;
+			rt = musexpr.right.dur;
+			t += ((lt > rt) ? lt : rt);
+			break;
+	}
+	notes.push(left);
+	notes.push(right);
+}
+
+function removenull(musexpr) {
+	var dexpr = [];
+	for (expr in musexpr) {
+		if (musexpr[expr] !== undefined && musexpr[expr] !== null) {
+			dexpr.push(musexpr[expr]);
+		}
+	}
+	return dexpr;
 }
 
 function compile(musexpr) {
-    return JSON.parse("[" + translate(musexpr) + "]");
+	var NOTE_CODE = [ ];
+	t = 0;
+	translate(musexpr, NOTE_CODE);
+	NOTE_CODE = removenull(NOTE_CODE);
+	return NOTE_CODE;
 }
 
 var melody_mus = 
     { tag: 'seq',
       left: 
-       { tag: 'seq',
+      { tag: 'seq',
          left: { tag: 'rest', dur: 250 },
-         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } 
+      },
       right:
-       { tag: 'seq',
-         left: { tag: 'rest', pitch: 'c4', dur: 500 },
-         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+      { tag: 'seq',
+         left: { tag: 'seq',
+			left: {tag: 'note', pitch: 'd7', dur: 250 },
+			right: { tag: 'seq', 
+				left: { tag: 'note', pitch: 'b4', dur: 250 },
+				right:{ tag: 'note', pitch: 'e1', dur: 250 }
+			}
+		 },
+         right: { tag: 'seq',
+			left: { tag: 'note', pitch: 'b4', dur: 250 },
+			right: {tag: 'rest', pitch: 'd7', dur: 500 }
+		 }
+	  }
+	  };
 
-/* rhino: let console.log be used instead of print */
+/* rhino || ie: let console.log be used instead of print */
 if (typeof console == "undefined" || typeof console.log == "undefined") var console = { log: function(g) {print(g);} };
 if (typeof print == "undefined") var console = { log: function(h) {} };
 
